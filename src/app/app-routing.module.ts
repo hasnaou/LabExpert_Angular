3cb8492c74ac14d59199a7f02e2e93d9fe5dd0b3
Talaini <mohatala@users.noreply.github.com>
2024-01-29 14:18:08 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PatientsComponent } from './patients/patients.component';
+import {FournisseurComponent} from "./fournisseur/fournisseur.component";
+import {AjoutFournisseurComponent} from "./fournisseur/ajout-fournisseur/ajout-fournisseur.component";
 
 const routes: Routes = [
-  {path: "patients", component: PatientsComponent}
+  {path: "patients", component: PatientsComponent},
+  {path: "fournisseurs", component: FournisseurComponent},
+  {path: "addfournisseur", component: AjoutFournisseurComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PatientsComponent } from './patients/patients.component';
+import { FournisseurComponent } from './fournisseur/fournisseur.component';
+import { AjoutFournisseurComponent } from './fournisseur/ajout-fournisseur/ajout-fournisseur.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    PatientsComponent
+    PatientsComponent,
+    FournisseurComponent,
+    AjoutFournisseurComponent
   ],
   imports: [
     BrowserModule,
